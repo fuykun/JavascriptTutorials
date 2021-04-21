2de3f9d65cf0bf97a272b05249b851d48e771a02
Furkan Uykun <frkan.uykun@hotmail.com>
2021-04-21 15:29:02 +0300

    update files.

---
+++ WebApi/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiForAjaxCalls
+{
+    public class Product
+    {
+        public Product()
+        {
+
+        }
+
+        public Product(int id, string name, decimal price, int quantity)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
